e71348d5cd6715a31d49a65be3edcda30519e34d
acnarf <88190874+acnarf@users.noreply.github.com>
2026-05-04 22:21:55 +0300

    Add files via upload

--- app/src/main/java/com/nimbusgida/bayiharitasi/MainActivity.java
+++ app/src/main/java/com/nimbusgida/bayiharitasi/MainActivity.java
@@ -1,58 +1,29 @@
 package com.nimbusgida.bayiharitasi;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.webkit.CookieManager;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.WebChromeClient;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    private WebView webView;
-    private static final String APP_URL = "https://nimbus-bayi-haritasi.firebaseapp.com/?app=android_v3";
+    private static final String APP_URL = "https://nimbus-bayi-haritasi.firebaseapp.com/?app=android_browser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        webView = new WebView(this);
-        webView.setLayoutParams(new FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.MATCH_PARENT
-        ));
-
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
-        settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(true);
-        settings.setAllowContentAccess(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setUseWideViewPort(true);
-        settings.setSupportZoom(false);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
-        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
-
-        CookieManager.getInstance().setAcceptCookie(true);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
-
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
-
-        setContentView(webView);
-        webView.loadUrl(APP_URL);
+        openInSecureBrowser();
     }
 
-    @Override
-    public void onBackPressed() {
-        if (webView != null && webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
+    private void openInSecureBrowser() {
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(APP_URL));
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+            Toast.makeText(this, "Tarayıcı açılamadı. Chrome yüklü olduğundan emin olun.", Toast.LENGTH_LONG).show();
+            finish();
         }
     }
 }
